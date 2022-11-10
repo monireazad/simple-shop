a3ee3be75983e4a11db44b2b33232d70c98f2eec
Monireh Azad <mim56azad@gmail.com>
2022-11-10 16:53:31 +0330

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <side-navigation/>
+    <top-navigation/>
+    <v-main class="base">
+      <v-container>
+        <Nuxt/>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import SideNavigation from "~/components/navigation/SideNavigation";
+import TopNavigation from "~/components/navigation/TopNavigation";
+
+export default {
+  name: 'DefaultLayout',
+  components: {TopNavigation, SideNavigation},
+  data() {
+    return {}
+  },
+  created() {
+    if (process.client){
+      this.$store.dispatch("initOrders")
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <h1>this is index page</h1>
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
